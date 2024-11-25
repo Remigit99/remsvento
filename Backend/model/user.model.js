4d6e5e1041a4314820e33d41ea7ae4afec7e6619
Aderemi Abiodun <aderemibiodun1000@gmail.com>
2024-11-25 14:08:19 +0100

    setup frontend and backend for the app

---
+++ Backend/model/user.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose"
+
+const userSchema=  new mongoose.Schema(
+    {
+        name:{
+            type: String,
+            require: [true, "Name is required"]
+        },
+        email: {
+            type: String,
+            require: [true, "Email is required"],
+            unique: true
+        },
+        password:{
+            type: String,
+            require: [true, "Password is required"]
+        }
+    },
+    {
+        timestamps: true
+    }
+)
